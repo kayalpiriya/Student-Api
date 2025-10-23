14ea4965a9fcaf10f767120090bd736e4df8010c
kayalpiriya09 <sdilakshana1@gmail.com>
2025-10-23 12:37:23 +0530

    vercel file added

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ import { connectDB } from "./src/config/mongoose.js";
 
 await connectDB('mongodb+srv://kayalpiriya_09:kayal2004@kayalpiriya.d4mp54n.mongodb.net/?retryWrites=true&w=majority&appName=kayalpiriya');
 app.listen(5000, () =>
-console.log(`🚀 Server running on http://localhost:${process.env.PORT}`)
+console.log(`🚀 Server running on http://localhost:5000`)
 );
