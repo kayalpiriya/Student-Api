bb08c4f72a2a465506e5d64eb767679d2586c815
kayalpiriya09 <sdilakshana1@gmail.com>
2025-10-23 12:09:13 +0530

    mongodb URL Changed

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ config(); // load .env
 import app from "./src/app.js";
 import { connectDB } from "./src/config/mongoose.js";
 
-await connectDB(process.env.MONGO_URI);
+await connectDB('mongodb+srv://kayalpiriya_09:kayal2004@kayalpiriya.d4mp54n.mongodb.net/?retryWrites=true&w=majority&appName=kayalpiriya');
 app.listen(process.env.PORT, () =>
 console.log(`🚀 Server running on http://localhost:${process.env.PORT}`)
 );
